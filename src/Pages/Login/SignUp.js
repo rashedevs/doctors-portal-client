030e9dbbabb613b99dc7931ce448bb5fcf2e3cda
rashedevs <rasheduap2015@gmail.com>
2022-05-15 17:36:48 +0600

    using put method for user sign up

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -8,6 +8,7 @@ import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
 import { Link, useNavigate } from "react-router-dom";
+import useToken from "../../hooks/useToken";
 
 const SignUp = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -21,6 +22,9 @@ const SignUp = () => {
     useCreateUserWithEmailAndPassword(auth);
 
   const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+  const [token] = useToken(gUser || user);
+
   const navigate = useNavigate();
 
   if (loading || gLoading || updating) {
@@ -36,14 +40,14 @@ const SignUp = () => {
       </p>
     );
   }
-  if (gUser || user) {
-    console.log(gUser || user);
+  if (token) {
+    // console.log(gUser || user);
+    navigate("/appointment");
   }
   const onSubmit = async (data) => {
     // console.log(data);
     await createUserWithEmailAndPassword(data.email, data.password);
     await updateProfile({ displayName: data.name });
-    navigate("/appointment");
   };
   return (
     <div className="flex h-screen justify-center items-center">
