f84cc6333a3b9339125d53809346133734482efa
rashedevs <rasheduap2015@gmail.com>
2022-05-16 13:26:06 +0600

    using functional button to create admin

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { toast } from "react-toastify";
 
-const UserRow = ({ user, index }) => {
+const UserRow = ({ user, refetch }) => {
+  const { email, role } = user;
+  const makeAdmin = () => {
+    fetch(`http://localhost:5000/user/admin/${email}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        refetch();
+        toast.success(`Successfully made an admin`);
+      });
+  };
   return (
     <tr>
       <th>1</th>
-      <td>{user.email}</td>
-      <td>Quality Control Specialist</td>
-      <td>Blue</td>
+      <td>{email}</td>
+      <td>
+        {role !== "admin" && (
+          <button onClick={makeAdmin} class="btn btn-xs">
+            Make Admin
+          </button>
+        )}
+      </td>
+      <td>
+        <button class="btn btn-xs">Remove User</button>
+      </td>
     </tr>
   );
 };
-
 export default UserRow;
