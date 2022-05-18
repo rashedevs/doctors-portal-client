92abda5dbef596c7492db027b4d8cd7c3f1391a8
rashedevs <rasheduap2015@gmail.com>
2022-05-18 15:49:12 +0600

    adding deleting modal on manage doctors page

--- src/Pages/Dashboard/DoctorRow.js
+++ src/Pages/Dashboard/DoctorRow.js
@@ -1,26 +1,8 @@
 import React from "react";
-import { toast } from "react-toastify";
 
-const DoctorRow = ({ doctor, index, refetch }) => {
-  const { name, specialty, img, email } = doctor;
-  const handleDelete = (email) => {
-    //use confirm modal before delete(hw)
-    const url = `http://localhost:5000/doctor/${email}`;
-    fetch(url, {
-      method: "DELETE",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.deletedCount === 1) {
-          toast.success(`Doctor :${name} is deleted`);
-          refetch();
-        }
-      });
-  };
+const DoctorRow = ({ doctor, index, refetch, setDeletingDoctor }) => {
+  const { name, specialty, img } = doctor;
+
   return (
     <tr>
       <th>{index + 1}</th>
@@ -34,12 +16,19 @@ const DoctorRow = ({ doctor, index, refetch }) => {
       <td>{name}</td>
       <td>{specialty}</td>
       <td>
-        <button
+        <label
+          onClick={() => setDeletingDoctor(doctor)}
+          for="delete-confirm-modal"
+          class="btn btn-xs btn-error"
+        >
+          Delete
+        </label>
+        {/* <button
           onClick={() => handleDelete(email)}
           class="btn btn-xs btn-error"
         >
           Delete
-        </button>
+        </button> */}
       </td>
     </tr>
   );
